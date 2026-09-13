db1408d77136a76fee81ced0a58e2850bfa597cd
itjasv <itjasv@gmail.com>
2026-09-13 22:46:39 +0530

    Fix invisible date picker on mobile browsers

--- src/app/report/page.js
+++ src/app/report/page.js
@@ -248,7 +248,7 @@ export default function ReportPage() {
 
               <div className="form-group">
                 <label className="label">When did it happen?</label>
-                <input type="date" className={`input ${styles.dateInput}`} value={form.incident_date} onChange={(e) => update("incident_date", e.target.value)} />
+                <input type="date" className="input" value={form.incident_date} onChange={(e) => update("incident_date", e.target.value)} />
               </div>
 
               <div className="form-group">
